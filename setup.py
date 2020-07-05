bc75f1718c1db4a09c6c3c727607761d0df9681e
dojo <sharadcodes>
2020-07-05 21:04:08 +0530

    Fix

--- setup.py
+++ setup.py
@@ -18,7 +18,7 @@ setup(
         "Programming Language :: Python :: 3",
         "Programming Language :: Python :: 3.6",
         "Programming Language :: Python :: 3.7",
-        "License :: OSI Approved :: GNU General Public License v3",
+        "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
         "Operating System :: OS Independent"
     ],
     python_requires='>=3.6'
